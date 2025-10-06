ac32afe2b1df636ecfb55273ef92923e3b33fbd7
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-10-06 11:13:48 +0200

    Update trend.py

--- trend.py
+++ trend.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv('XBTUSD_daily.csv', parse_dates=['date'])
+df = pd.read_csv('btc_daily.csv', parse_dates=['date'])
 df['SMA20'] = df['close'].rolling(20).mean()
 df['trend'] = df['close'].gt(df['SMA20']).map({True: 'UP', False: 'DOWN'})
 print(df[['date','close','trend']].tail())
