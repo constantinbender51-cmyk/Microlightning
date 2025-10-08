ded691eb7cdf30f79d8d24ad841fe4d67cce8407
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-10-08 12:46:09 +0200

    Update macd.py

--- macd.py
+++ macd.py
@@ -1,4 +1,4 @@
-import pandas as pd
+wimport pandas as pd
 import numpy as np
 import time
 
@@ -34,7 +34,7 @@ for i in range(1, len(df)):
     
     if in_pos != 0 and ((entry_p/df['high'].iloc[i]-1)*in_pos>=stp_pct or (entry_p/df['low'].iloc[i]-1)*in_pos>=stp_pct):
       stp = True
-      stp_price=curve[0] * (1 + (entry_p*(1-stp_pct)/entry_p - 1) * in_pos * LEVERAGE)
+      stp_price=curve[-1] * (1 + (entry_p*(1-stp_pct)/entry_p - 1) * in_pos * LEVERAGE)
       
     # ----- entry logic --------------------------------------------------------
     if in_pos == 0 and pos_i != 0:
@@ -49,7 +49,7 @@ for i in range(1, len(df)):
     if in_pos != 0 and pos_i == -in_pos:
         ret = (p_now / entry_p - 1) * in_pos * LEVERAGE
         if stp == True:
-          trades.append((entry_d, df['date'].iloc[i], stp_price/curve[0]-1)
+          trades.append((entry_d, df['date'].iloc[i], stp_price/curve[-1]-1)
         else:
           trades.append((entry_d, df['date'].iloc[i], ret))
         in_pos = 0
